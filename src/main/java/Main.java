07b1e185e2cc66aeb062e7a2ca385cc74c5ca168
Mihaela Topolnicianu <mtopolnicianu@deloittece.com>
2023-08-02 11:08:48 +0300

    Java Arrays - Practice - added new exercises

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        JavaArraysExercises.arraysChallengeExercise2();
+        JavaArraysExercises.arraysTwoDimensionalExercise();
 
 //        JavaIfElseAndSwitchForWhileExercises.firstExercise();
 //        JavaIfElseAndSwitchForWhileExercises.secondExercise();
