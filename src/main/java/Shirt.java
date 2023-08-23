dda63415cc5c64b2ab2dc0e772842cdf26e24af2
Mihaela Topolnicianu <mtopolnicianu@deloittece.com>
2023-08-23 10:20:57 +0300

    Java Constructors Practical - added new exercises

---
+++ src/main/java/Shirt.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Shirt {
+
+    public static String color;
+    public static char size;
+    public static String texture;
+
+    Shirt() {
+
+    }
+
+    Shirt(String newColor, char newSize) {
+        color = newColor;
+        size = newSize;
+    }
+
+    Shirt(String newTexture, char anotherSize, String anotherColor) {
+        texture = newTexture;
+        size = anotherSize;
+        color = anotherColor;
+    }
+
+    //
+//    Shirt(){
+//        System.out.println("inside constructor!");
+//    }
+    public static void putOn() {
+        System.out.println("Shirt is on!");
+    }
+
+    public static void takeOff() {
+        System.out.println("Short is off");
+    }
+
+    public static void setColor(String newColor) {
+        color = newColor;
+    }
+
+    public static void setSize(char newSize) {
+        size = newSize;
+    }
+
+    public static void main(String[] args) {
+        Shirt shirt = new Shirt("White", 'M');
+        Shirt newShirt = new Shirt("Blue", 'S');
+//        shirt.setColor("White");
+//        shirt.setSize('M');
+//        shirt.putOn();
+
+        System.out.println(shirt.color);
+        System.out.println(shirt.size);
+
+        Scanner scanner = new Scanner(System.in);
+    }
+}
