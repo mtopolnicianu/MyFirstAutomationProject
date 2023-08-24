b0ebbdbd8476206c5b45c9c1ca46b0815b13da35
Mihaela Topolnicianu <mtopolnicianu@deloittece.com>
2023-08-24 09:27:40 +0300

    Java Polymorphism Practical - added new exercises

---
+++ src/main/java/Polymorphism/Shapes.java
@@ -0,0 +1,17 @@
+package Polymorphism;
+
+public class Shapes {
+    public void area() {
+        System.out.println("The formula for area of ");
+    }
+
+    public static void main(String[] args) {
+        Shapes myShape = new Shapes();  // Create a Shapes object
+        Shapes myTriangle = new Triangle();  // Create a Triangle object
+        Shapes myCircle = new Circle();  // Create a Circle object
+        myShape.area();
+        myTriangle.area();
+        myShape.area();
+        myCircle.area();
+    }
+}
